853dae3b876f50ae887f76551416ae3764628658
azuldeprusia06 <soynayeligarciajuarez@gmail.com>
2025-08-28 20:04:16 -0600

    Nuevo Ejercicio

---
+++ Ejercicios/ParoImpar.py
@@ -0,0 +1,14 @@
+"""
+Escribe un programa en Python que muestre los números del 1 al 50, indicando junto a cada número si es par o impar:
+
+
+Ejemplo:
+
+1 es impar  
+2 es par  
+3 es impar  
+4 es par  
+...  
+50 es par
+
+"""
