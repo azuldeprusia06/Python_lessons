700d25cb7c95a9393310f7ce3fc5957e1e8d5cf7
azuldeprusia06 <soynayeligarciajuarez@gmail.com>
2025-08-28 19:19:49 -0600

    Agregado de carpeta y ejercicio

---
+++ Ejercicios/FizzBuzz.py
@@ -0,0 +1,32 @@
+"""
+Enunciado
+Escribe un programa en Python que muestre los números del 1 al 100, pero aplicando estas reglas:
+Si el número es múltiplo de 3, en lugar del número debe mostrar la palabra "Fizz".
+
+
+Si el número es múltiplo de 5, en lugar del número debe mostrar la palabra "Buzz".
+
+
+Si el número es múltiplo tanto de 3 como de 5, debe mostrar la palabra "FizzBuzz".
+
+
+En cualquier otro caso, simplemente muestra el número.
+
+
+Ejemplo de salida esperada
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+...
+14
+FizzBuzz
+16
+
+"""
