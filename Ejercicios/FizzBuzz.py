9b8e3a8057e88795e8549ed0716a7e0b0d80e9da
azuldeprusia06 <soynayeligarciajuarez@gmail.com>
2025-08-28 19:51:52 -0600

    Fizzbuzz resuelto

--- Ejercicios/FizzBuzz.py
+++ Ejercicios/FizzBuzz.py
@@ -1,32 +1,27 @@
 """
-Enunciado
+Enunciado:
+
 Escribe un programa en Python que muestre los números del 1 al 100, pero aplicando estas reglas:
-Si el número es múltiplo de 3, en lugar del número debe mostrar la palabra "Fizz".
+
+- Si el número es múltiplo de 3, en lugar del número debe mostrar la palabra "Fizz".
+- Si el número es múltiplo de 5, en lugar del número debe mostrar la palabra "Buzz".
+- Si el número es múltiplo tanto de 3 como de 5, debe mostrar la palabra "FizzBuzz".
+- En cualquier otro caso, simplemente muestra el número.
+""" 
 
 
-Si el número es múltiplo de 5, en lugar del número debe mostrar la palabra "Buzz".
+numeros= list(range (1,101))
 
 
-Si el número es múltiplo tanto de 3 como de 5, debe mostrar la palabra "FizzBuzz".
+def fizzbuzz(numeros):
+    for numero in numeros:
+        if numero % 3== 0 and numero % 5 == 0:
+            print("FizzBuzz")
+        elif numero % 3 == 0:
+            print("Fizz")
+        elif numero % 5 == 0:
+            print("Buzz")
 
+        else: print(numero)
 
-En cualquier otro caso, simplemente muestra el número.
-
-
-Ejemplo de salida esperada
-1
-2
-Fizz
-4
-Buzz
-Fizz
-7
-8
-Fizz
-Buzz
-...
-14
-FizzBuzz
-16
-
-"""
+fizzbuzz(numeros)
