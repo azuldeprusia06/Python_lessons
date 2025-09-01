19e9b9f576428476e0c80bcfe680d98a3c57d3bb
azuldeprusia06 <soynayeligarciajuarez@gmail.com>
2025-09-01 17:31:47 -0600

    Resultado_calculadora

--- Ejercicios/Calculadora.py
+++ Ejercicios/Calculadora.py
@@ -34,3 +34,15 @@ La función principal no debería hacer los cálculos directamente, sino delegar
 Puedes usar condicionales (if/elif) para decidir qué función llamar.
 
 Recuerda manejar la división para evitar errores si el divisor es 0.
+number_1:input(float"Ingresa el primer número")
+number_2: input(float"Ingresa el segundo número")
+operation:input("Ingresa la operacion (suma, resta, multiplicación o división)")
+
+def calculadora(number_1,number_2)
+    if operation == suma == number_1 + number_2
+    elif operation ==resta == number_1 - number_2
+    elif operation == multiplicación == number_1*number_2
+    elif operation == división == number_1/number_2
+    elif operation == división and number_2 == 0 
+        print("Error, no se puede dividir entre 0")
+    else pritn("Valor no valido")
